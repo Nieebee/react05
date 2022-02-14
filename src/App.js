0cc41d446725cb60603b62d66c1bfc13ffcde6b6
Niédja Bezerra <niedjacarlos@gmail.com>
2022-02-14 01:27:59 -0300

    react 05 - Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    students: [
+      {
+        name: "Pedro",
+      },
+      {
+        name: "Thiago",
+      },
+      {
+        name: "João",
+      },
+      {
+        name: "Miguel",
+      },
+      {
+        name: "Paulo",
+      },
+      {
+        name: "Renan",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <p>Lista de alunos:</p>
+        {this.state.students.map((name) => (
+          <div>
+            <p>{name.name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default App;
